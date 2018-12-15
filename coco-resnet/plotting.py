a839b1c77564f63d037fbf35ead71031c93fbff9
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-12-15 00:48:10 -0600

    Add coco resnet

---
+++ coco-resnet/plotting.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+import csv
+import statistics as stat
+
+n_bins = 20
+
+# Get data and compute times
+data_times = []
+compute_times = []
+with open("cifar10-resnet152.csv") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        data_times.append(float(row[0]))
+        compute_times.append(float(row[1]))
+
+
+fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+axs[0].set_title("Data Time")
+axs[0].set_xlabel("Frequency")
+axs[0].set_ylabel("Time")
+axs[1].set_title("Compute Time")
+axs[1].set_xlabel("Frequency")
+axs[1].set_ylabel("Time")
+
+axs[0].hist(data_times, bins=50, range=(0.5, 1.0))
+axs[1].hist(compute_times, bins=50, range=(0.5, 1.0))
+print("Data Time statistics:")
+print("Mean = " + str(stat.mean(data_times)))
+print("Median = " + str(stat.median(data_times)))
+
+print("Standard Deviation = " + str(stat.stdev(data_times)))
+print("Compute Time statistics:")
+print("Mean = " + str(stat.mean(compute_times)))
+print("Median = " + str(stat.median(compute_times)))
+print("Standard Deviation = " + str(stat.stdev(compute_times)))
+
+
+plt.savefig("histogram.png")
---
+++ coco-resnet/train.py
@@ -0,0 +1,64 @@
+import csv
+import time
+import torch
+import torch.nn as nn
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+# Data
+transform = transforms.Compose(
+    [transforms.Resize([224, 224]),
+     transforms.ToTensor()])
+
+train_dataset = datasets.CocoDetection(
+    root='../../../data/cocostuff/dataset/images/train2017',
+    annFile='../../../data/cocostuff/dataset/annotations/stuff_train2017.json',
+    transform=transform)
+
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset, batch_size=3, shuffle=True)
+
+# Model
+model = models.resnet18(pretrained=False)
+
+# Train
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = model.to(device)
+
+num_epochs = 2
+learning_rate = 0.001
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+total_step = len(train_loader)
+curr_lr = learning_rate
+for epoch in range(num_epochs):
+    step = 0
+    data_times = []
+    compute_times = []
+    start = time.time()
+    for images, labels in train_loader:
+        data_times.append(time.time() - start)
+        start = time.time()
+        step += 1
+
+        images = images.to(device)
+        labels = torch.ones([3, 1000])
+        labels = labels.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        compute_times.append(time.time() - start)
+        print("Step = " + str(step) + " out of " + str(total_step))
+
+    with open('logs.csv', 'w', newline='') as logfile:
+        logwriter = csv.writer(
+            logfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        for row in zip(data_times, compute_times):
+            logwriter.writerow(row)
